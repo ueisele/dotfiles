583d59beaecc8edce597005029357a323c47aa4d
Uwe Eisele <code@uweeisele.eu>
2020-08-16 11:32:01 +0200

    WIP: add install script for jq and yq

--- yq/install.sh
+++ yq/install.sh
@@ -8,30 +8,19 @@ source ${ROOT_DIR}/env.sh
 source ${ROOT_DIR}/function.log.sh
 popd > /dev/null
 
-GITHUB_REPO="sharkdp/bat"
-
-# notes:
-# - https://github.com/sharkdp/bat
-# - https://github.com/eth-p/bat-extras
-# - https://github.com/dandavison/delta
-# - https://github.com/burntsushi/ripgrep
+GITHUB_REPO="mikefarah/yq"
 
 function resolve_arch_type () {
-    uname -m
-}
-
-function resolve_libc_type () { 
-    if (ldd --version | grep -q -i musl); then echo musl; else echo gnu; fi
+    if (uname -m | grep -q x86_64); then echo amd64; else uname -m; fi
 }
 
 function resolve_download_url () {
     local tag=${1:-"latest"}
     local arch_type=${2:-$(resolve_arch_type)}
-    local libc_type=${3:-$(resolve_libc_type)}
 
     local query=$(if [ ${tag} == "latest" ]; then echo ${tag}; else echo "tags/${tag}"; fi)
     
-    curl -sL https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/' | grep ${arch_type} | grep ${libc_type}
+    curl -sL https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/' | grep linux | grep ${arch_type}
 }
 
 function resolve_actual_tag () {
@@ -50,12 +39,11 @@ function create_target_dir () {
 function download () {
     local tag=${1:-"latest"}
     local arch_type=${2:-$(resolve_arch_type)}
-    local libc_type=${3:-$(resolve_libc_type)}
-    local target="${4:-${DOTFILES_BIN_DIR}}"
+    local target="${3:-${DOTFILES_BIN_DIR}}"
 
-    local download_url="$(resolve_download_url ${tag} ${arch_type} ${libc_type})"   
+    local download_url="$(resolve_download_url ${tag} ${arch_type})"   
     if [ -z "${download_url}" ]; then
-        fail 1 "Could not determine an artifact for arch '${arch_type}' and libc '${libc_type}' with tag '${tag}'."
+        fail 1 "Could not determine an artifact for arch '${arch_type}' with tag '${tag}'."
     fi
 
     if [ ! -d ${target} ]; then
@@ -64,15 +52,14 @@ function download () {
     fi
 
     local actual_tag="$(resolve_actual_tag ${tag})"
-    local filename_short="bat"
+    local filename_short="yq"
     local filename_full="${filename_short}-${actual_tag}"
     if [ -f "${target}/${filename_full}" ]; then
         log "Artifact ${target}/${filename_full} has already been downloaded."
     else
-        log "INFO" "Download artifact for arch '${arch_type}' and libc '${libc_type}' with tag '${actual_tag}' from URL ${download_url}" 
-        #wget "${download_url}"" -qO - | tar -xz -C "${target}" --overwrite --strip-components=1 --wildcards "*/bat"
-        curl -sfL "${download_url}" | tar -xz -C "${target}" --overwrite --strip-components=1 --wildcards "*/${filename_short}"
-        mv -f "${target}/${filename_short}" "${target}/${filename_full}"
+        log "INFO" "Download artifact for arch '${arch_type}' with tag '${actual_tag}' from URL ${download_url}" 
+        curl -sfLR -o "${target}/${filename_full}" "${download_url}"
+        chmod +x "${target}/${filename_full}"
         log "INFO" "Artifact has been downloaded to ${target}/${filename_full}"
     fi
 
