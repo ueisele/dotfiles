8d5c9b7143eb8c9834bb1d3fc8a6b6c93f35c501
Uwe Eisele <code@uweeisele.eu>
2020-10-12 18:53:54 +0200

    exa: refactor install

--- exa/install.sh
+++ exa/install.sh
@@ -8,6 +8,8 @@ source ${ROOT_DIR}/function.os.sh
 
 GITHUB_REPO="ogham/exa"
 
+RETRIES=3
+
 function ensure_downloaded_and_installed_from_github () {
     local tag=${1:-"latest"}
     local arch_type=${2:-$(resolve_arch_type)}
@@ -22,27 +24,27 @@ function ensure_downloaded_and_installed_from_github () {
     local name_short="exa"
     local name_full="${name_short}-${actual_tag}"
 
-    if [ -f "${DOTFILES_APP_DIR}/${name_full}/${name_short}" ]; then
+    if [ -f "${DOTFILES_APP_DIR}/${name_full}/download.timestamp" ]; then
         log "INFO" "Artifact ${name_full} has already been downloaded."
     else
         log "INFO" "Download artifact for arch '${arch_type}' and os '${os_type}' with tag '${actual_tag}' from URL ${download_url}"
         mkdir -p "${DOTFILES_APP_DIR}/${name_full}"
         local tmpfile="$(mktemp)"
-        curl -sfLR -o "${tmpfile}" "${download_url}"
+        curl -sfLR --retry ${RETRIES} -o "${tmpfile}" "${download_url}"
         unzip -o -d "${DOTFILES_APP_DIR}/${name_full}" "${tmpfile}"
         rm "${tmpfile}"
         mv -f "${DOTFILES_APP_DIR}/${name_full}/${name_short}-${os_type}-${arch_type}" "${DOTFILES_APP_DIR}/${name_full}/${name_short}"        
         log "INFO" "Artifact has been downloaded to ${DOTFILES_APP_DIR}/${name_full}"
 
-        log "INFO" "Download man page for tag '${actual_tag}' from URL https://raw.githubusercontent.com/${GITHUB_REPO}/${actual_tag}/contrib/man/${name_short}.1"
+        log "INFO" "Download man page for tag ${actual_tag}"
         mkdir -p "${DOTFILES_APP_DIR}/${name_full}/man"
-        curl -sfLR -o "${DOTFILES_APP_DIR}/${name_full}/man/${name_short}.1" "https://raw.githubusercontent.com/${GITHUB_REPO}/${actual_tag}/contrib/man/${name_short}.1"
-        log "INFO" "Downloaded man page to ${DOTFILES_APP_DIR}/${name_full}/man/${name_short}.1"
+        download_github_raw_content "${GITHUB_REPO}" "${actual_tag}" "contrib/man" "${DOTFILES_APP_DIR}/${name_full}/man" "${name_short}.1"
 
-        log "INFO" "Download ZSH autocompletion for tag '${actual_tag}' from URL https://raw.githubusercontent.com/${GITHUB_REPO}/${actual_tag}/contrib/completions.zsh"
+        log "INFO" "Download ZSH autocompletion for tag ${actual_tag}"
         mkdir -p "${DOTFILES_APP_DIR}/${name_full}"
-        curl -sfLR -o "${DOTFILES_APP_DIR}/${name_full}/completions.zsh" "https://raw.githubusercontent.com/${GITHUB_REPO}/${actual_tag}/contrib/completions.zsh"
-        log "INFO" "Downloaded ZSH auto completion file to ${DOTFILES_APP_DIR}/${name_full}/completions.zsh"
+        download_github_raw_content "${GITHUB_REPO}" "${actual_tag}" "contrib" "${DOTFILES_APP_DIR}/${name_full}" "completions.zsh"
+
+        echo "$(date -Isec)" > "${DOTFILES_APP_DIR}/${name_full}/download.timestamp"
     fi
 
     mkdir -p "${DOTFILES_BIN_DIR}"
@@ -95,6 +97,20 @@ function resolve_actual_tag () {
     local query=$(if [ ${tag} == "latest" ]; then echo ${tag}; else echo "tags/${tag}"; fi)
     
     curl $(resolve_github_credentials) -sL https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+function download_github_raw_content () {
+    local repo="${1:?Requires GitHub repo as first parameter!}"
+    local tag="${2:?Requires tag as second parameter!}"
+    local sourcedir="${3:?Requires source dir as third parameter!}"
+    local targetdir="${4:?Requires target dir as fourth parameter!}"
+    local file="${5:?Requires file as fith parameter!}"
+    local mod="${6:-""}"
+    curl -sfLR --retry ${RETRIES} -o "${targetdir}/${file}" "https://raw.githubusercontent.com/${repo}/${tag}/${sourcedir}/${file}"
+    if [ -n "${mod}" ]; then
+        chmod "${mod}" "${targetdir}/${file}"
+    fi
+    log "INFO" "Downloaded ${sourcedir}/${file} from GitHub Repo ${repo}:${tag} to ${targetdir}/${file}"
 }
 
 function resolve_github_credentials () {
