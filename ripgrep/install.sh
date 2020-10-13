383ebe7d929841ba1ab8d4f5cf96a1b5da802a00
Uwe Eisele <code@uweeisele.eu>
2020-10-13 19:14:35 +0200

    ripgrep: refactor install and add man page

--- ripgrep/install.sh
+++ ripgrep/install.sh
@@ -7,10 +7,44 @@ source ${ROOT_DIR}/function.log.sh
 
 GITHUB_REPO="BurntSushi/ripgrep"
 
-function resolve_github_credentials () {
-    if [ -n "${GITHUB_USER}" ] && [ -n "${GITHUB_TOKEN}" ]; then
-        echo "-u ${GITHUB_USER}:${GITHUB_TOKEN}"
+RETRIES=3
+
+function ensure_downloaded_and_installed_from_github () {
+    local tag=${1:-"latest"}
+    local arch_type=${2:-$(resolve_arch_type)}
+    local os_type=${3:-$(resolve_os_type)}
+
+    local download_url="$(resolve_download_url ${tag} ${arch_type} ${os_type})"   
+    if [ -z "${download_url}" ]; then
+        fail 1 "Could not determine an artifact for arch '${arch_type}' and os '${os_type}' with tag '${tag}'."
     fi
+
+    local actual_tag="$(resolve_actual_tag ${tag})"
+    local name_short="rg"
+    local name_full="${name_short}-${actual_tag}"
+    if [ -f "${DOTFILES_APP_DIR}/${name_full}/download.timestamp" ]; then
+        log "INFO" "Artifact ${name_full} has already been downloaded."
+    else
+        log "INFO" "Download artifact for arch '${arch_type}' and os '${os_type}' with tag '${actual_tag}' from URL ${download_url}"
+        mkdir -p "${DOTFILES_APP_DIR}/${name_full}"
+        curl -sfL --retry ${RETRIES} "${download_url}" | tar -xz -C "${DOTFILES_APP_DIR}/${name_full}" --overwrite --strip-components=1
+        chown -R $(id -u):$(id -g) "${DOTFILES_APP_DIR}/${name_full}"
+        log "INFO" "Artifact has been downloaded to ${DOTFILES_APP_DIR}/${name_full}"
+
+        echo "$(date -Isec)" > "${DOTFILES_APP_DIR}/${name_full}/download.timestamp"
+    fi
+
+    mkdir -p "${DOTFILES_BIN_DIR}"
+	ln -srf "${DOTFILES_APP_DIR}/${name_full}/${name_short}" "${DOTFILES_BIN_DIR}/${name_short}"
+    log "INFO" "Linked binary from ${DOTFILES_APP_DIR}/${name_full}/${name_short} to ${DOTFILES_BIN_DIR}/${name_short}"
+
+    mkdir -p "${DOTFILES_MAN_DIR}/man1"
+    ln -srf "${DOTFILES_APP_DIR}/${name_full}/doc/${name_short}.1" "${DOTFILES_MAN_DIR}/man1/${name_short}.1"
+    log "INFO" "Linked man page from ${DOTFILES_APP_DIR}/${name_full}/doc/${name_short}.1 to ${DOTFILES_MAN_DIR}/man1/${name_short}.1"
+
+    mkdir -p "${DOTFILES_COMPLETIONS_ZSH_DIR}"
+    ln -srf  "${DOTFILES_APP_DIR}/${name_full}/complete/_${name_short}" "${DOTFILES_COMPLETIONS_ZSH_DIR}/_${name_short}"
+    log "INFO" "Linked ZSH auto completion from ${DOTFILES_APP_DIR}/${name_full}/complete/_${name_short} to ${DOTFILES_COMPLETIONS_ZSH_DIR}/_${name_short}"
 }
 
 function resolve_arch_type () {
@@ -20,8 +54,6 @@ function resolve_arch_type () {
 function resolve_os_type () {
     if [[ "$OSTYPE" == "linux"* ]]; then
         echo "linux-musl"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "apple-darwin"
     else
         echo "$OSTYPE"
     fi
@@ -34,7 +66,7 @@ function resolve_download_url () {
 
     local query=$(if [ ${tag} == "latest" ]; then echo ${tag}; else echo "tags/${tag}"; fi)
     
-    curl $(resolve_github_credentials) -sL https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/' | grep ${arch_type} | grep ${os_type}
+    curl $(resolve_github_credentials) -sL --retry ${RETRIES} https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/' | grep ${arch_type} | grep ${os_type}
 }
 
 function resolve_actual_tag () {
@@ -42,52 +74,13 @@ function resolve_actual_tag () {
 
     local query=$(if [ ${tag} == "latest" ]; then echo ${tag}; else echo "tags/${tag}"; fi)
     
-    curl $(resolve_github_credentials) -sL https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+    curl $(resolve_github_credentials) -sL --retry ${RETRIES} https://api.github.com/repos/${GITHUB_REPO}/releases/${query} | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-function create_target_dir () {
-    local target="${1:?Missing target dir as first parameter!}"
-    mkdir -p "${target}"
+function resolve_github_credentials () {
+    if [ -n "${GITHUB_USER}" ] && [ -n "${GITHUB_TOKEN}" ]; then
+        echo "-u ${GITHUB_USER}:${GITHUB_TOKEN}"
+    fi
 }
 
-function download () {
-    local tag=${1:-"latest"}
-    local arch_type=${2:-$(resolve_arch_type)}
-    local os_type=${3:-$(resolve_os_type)}
-    local target="${4:-${DOTFILES_BIN_DIR}}"
-
-    local download_url="$(resolve_download_url ${tag} ${arch_type} ${os_type})"   
-    if [ -z "${download_url}" ]; then
-        fail 1 "Could not determine an artifact for arch '${arch_type}' and os '${os_type}' with tag '${tag}'."
-    fi
-
-    if [ ! -d ${target} ]; then
-        log "INFO" "Create ${target} as target directory for binaries"
-        create_target_dir "${target}"
-    fi
-
-    local actual_tag="$(resolve_actual_tag ${tag})"
-    local filename_short="rg"
-    local filename_full="${filename_short}-${actual_tag}"
-    if [ -f "${target}/${filename_full}" ]; then
-        log "INFO" "Artifact ${target}/${filename_full} has already been downloaded."
-    else
-        log "INFO" "Download artifact for arch '${arch_type}' and os '${os_type}' with tag '${actual_tag}' from URL ${download_url}"
-        local tmpdir="$(mktemp -d)"
-        curl -sfL "${download_url}" | tar -xz -C "${tmpdir}" --overwrite --strip-components=1
-        chown -R $(id -u):$(id -g) "${tmpdir}"
-        mv -f "${tmpdir}/${filename_short}" "${target}/${filename_full}"
-        log "INFO" "Artifact has been downloaded to ${target}/${filename_full}"
-
-        mkdir -p "${DOTFILES_COMPLETIONS_ZSH_DIR}"
-        mv -f "${tmpdir}/complete/_${filename_short}" "${DOTFILES_COMPLETIONS_ZSH_DIR}/_${filename_short}"
-        log "INFO" "Created ZSH auto completion file ${DOTFILES_COMPLETIONS_ZSH_DIR}/_${filename_short}"
-
-        rm -r "${tmpdir}"
-    fi
-
-    ln -sf "${target}/${filename_full}" "${target}/${filename_short}"
-    log "INFO" "Created symlink from ${target}/${filename_full} to ${target}/${filename_short}"
-}
-
-download "$@"
+ensure_downloaded_and_installed_from_github "$@"
