c34744b80a62f41d7b7d820b34c09ae9e3ec190d
Uwe Eisele <code@uweeisele.eu>
2020-10-22 21:51:00 +0200

    exa: fix alias

--- exa/alias/exa.zsh
+++ exa/alias/exa.zsh
@@ -2,6 +2,10 @@ alias exa='exa --group-directories-first --git --icons --group'
 alias el='exa -l'
 alias ela='exa -la'
 
-alias ls='exa'
-alias ll='el'
-alias la='ela'
+function ls () {
+    # required because exa cannot list if symlinks are broken
+    exa $@ 2> /dev/null || /bin/ls -h $@
+}
+
+alias ll='ls -l'
+alias la='ls -la'
