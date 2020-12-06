8763eb9aef50c70742bcbd2f1e6f69ace326d121
Uwe Eisele <code@uweeisele.eu>
2020-12-06 16:27:52 +0100

    load asdf java plugin init if it exists

--- zsh/zprezto-modules/asdf/init.zsh
+++ zsh/zprezto-modules/asdf/init.zsh
@@ -1,5 +1,5 @@
 #
-# Configures direnv
+# Configures asdf
 #
 
 # Return if requirements are not found
@@ -7,4 +7,10 @@ if [ ! -f "${HOME}/.asdf/asdf.sh" ]; then
     return 1
 fi
 
+# Init asdf
 source "${HOME}/.asdf/asdf.sh"
+
+# Init specific asdf plugins
+if [[ -f "${HOME}/.asdf/plugins/java/set-java-home.zsh" ]]; then
+    source "${HOME}/.asdf/plugins/java/set-java-home.zsh"
+fi
