5fbaad13dab9e8e5e8f9ebaaca3350a372c27aa8
Uwe Eisele <code@uweeisele.eu>
2021-01-06 12:25:00 +0100

    git - set default git branch name to main

--- git/install.sh
+++ git/install.sh
@@ -12,8 +12,10 @@ function userSigningKey () {
 
 function ensure_git_is_configured () {
     log "INFO" "Configuring Git"
-    ## User
+    ## Init
+    git config --global init.defaultBranch main
 
+    ## User
     git config --global user.name "${USER_FULLNAME:-$(whoami)}"
     git config --global user.email "${USER_EMAIL:-$(whoami)@$(hostname)}"
 
@@ -26,20 +28,16 @@ function ensure_git_is_configured () {
     fi
 
     ## Core
-
     git config --global core.autocrlf input
     git config --global core.excludesfile '~/.gitignore-global'
 
     ## Pull
-
     git config --global pull.rebase true
 
     ## Push
-
     git config --global push.default simple
 
     ## Diff & Merge
-
     git config --global merge.conflictstyle diff3
     git config --global mergetool.prompt false
     git config --global mergetool.keepTemporaries false
