32bf0ceff83fefc873387c05b122809411b246f0
Uwe Eisele <code@uweeisele.eu>
2021-04-25 19:31:16 +0200

    fix locales setup

--- locales/install.sh
+++ locales/install.sh
@@ -10,14 +10,16 @@ function ensure_locales_are_installed () {
 		log "INFO" "Skipping installation of locales, because Alpine does not support locales"
 		return
 	fi
+
+	${INSTALL_PACKAGE_BIN} --install \
+		"debian=locales,ubuntu=locales" \
+		"fedora=glibc-locale-source,centos(>=8)=glibc-locale-source" "fedora(>=30)=langpacks-en,centos(>=8)=langpacks-en" "fedora(>=30)=glibc-langpack-en,centos(>=8)=glibc-langpack-en"
+
 	if command -v locale > /dev/null && locale -a | grep -q en_US.utf8; then
 		log "INFO" "Locale en_US.utf8 is already installed"
 		return
 	fi
 
-	${INSTALL_PACKAGE_BIN} --install \
-		"debian=locales,ubuntu=locales" \
-		"fedora=glibc-locale-source,centos(>=8)=glibc-locale-source" "fedora(>=30)=langpacks-en,centos(>=8)=langpacks-en" "fedora(>=30)=glibc-langpack-en,centos(>=8)=glibc-langpack-en"
 	if [ "$(current_os)" = "debian" ] || [ "$(current_os)" = "ubuntu" ]; then
 		log "INFO" "Generating en_US.UTF-8 locale"
 		run_with_sudo_if_required "sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen"
